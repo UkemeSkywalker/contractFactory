3c373d1eefa6896b6b0c439a312f98a691e81582
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-08-10 20:59:12 +0100

    update mapping, withdraw and approve functions

--- contracts/multisig.sol
+++ contracts/multisig.sol
@@ -6,6 +6,7 @@ contract Multisig {
 
     // A contract that allows 70% of ValidSigner to approve before a withrawal is successful.
     address[] validSigner;
+    uint ID;
     uint Quorum = (validSigner.length * 70) / 100;
     //mapping of transactionID to number of approval to status
     mapping (uint => mapping(uint => bool)) _approved;
@@ -13,12 +14,17 @@ contract Multisig {
     mapping (uint => uint) noOfApproval;    
     //checking id an addess gas approved a particular transactionID
     mapping (address => mapping(uint => bool)) signed;
+    mapping(uint => address) beneficiary;
+    mapping(uint => uint) amount;
     constructor(address[] memory _validSigner) {
         validSigner = _validSigner;
     }
 
     function withdrawEther(uint _amount) external {
         bool _valid = validOwners();
+        Approve(ID);
+        beneficiary[ID] = msg.sender;
+        amount[ID] = _amount;
     }
 
     function validOwners() private view returns(bool success){
@@ -34,10 +40,13 @@ contract Multisig {
 
     function Approve(uint id) public{
         bool valid = validOwners();
+        uint Value  = amount[id];
+        address _beneficiary = beneficiary[id];
         assert(signed[msg.sender][id] == false);
         uint num = noOfApproval[id]+ 1;
         if(num >= Quorum){
             _approved[id][num] = true;
+            payable(_beneficiary).transfer(Value);
         }
     }
 }
