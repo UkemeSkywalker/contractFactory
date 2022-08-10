2459acb25a66d9200c0c33d2425d4e55be1d4d8d
ukemeSkywalker <skywalkershadow9@gmail.com>
2022-08-10 20:41:06 +0100

    hardhat and multisig contract update

---
+++ contracts/multisig.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.4;
+
+contract Multisig {
+
+
+    // A contract that allows 70% of ValidSigner to approve before a withrawal is successful.
+    address[] validSigner;
+    uint Quorum = (validSigner.length * 70) / 100;
+    //mapping of transactionID to number of approval to status
+    mapping (uint => mapping(uint => bool)) _approved;
+    //mapping of transactionId to number of approval
+    mapping (uint => uint) noOfApproval;    
+    //checking id an addess gas approved a particular transactionID
+    mapping (address => mapping(uint => bool)) signed;
+    constructor(address[] memory _validSigner) {
+        validSigner = _validSigner;
+    }
+
+    function withdrawEther(uint _amount) external {
+        bool _valid = validOwners();
+    }
+
+    function validOwners() private view returns(bool success){
+        address valid;
+        for(uint i = 0;  i < validSigner.length; i++){
+            if(msg.sender == validSigner[i]){
+                valid = msg.sender;
+            }
+            require(msg.sender != address(0), "can not be address zero");
+        }
+        success = true;
+    }
+
+    function Approve(uint id) public{
+        bool valid = validOwners();
+        assert(signed[msg.sender][id] == false);
+        uint num = noOfApproval[id]+ 1;
+        if(num >= Quorum){
+            _approved[id][num] = true;
+        }
+    }
+}
